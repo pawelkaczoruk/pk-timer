3258934e79acf58d5a69e100ddecbb7bd1f52ffd
fishu <pkaczoruk@wp.pl>
2020-02-08 14:01:10 +0100

    add menu bar component with basicy styles. Add reset.scss and variables.scss files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-
+    <MenuBar />
   </div>
 </template>
 
 <script>
+import MenuBar from './components/MenuBar'
 
 export default {
   name: 'App',
   components: {
-
+    MenuBar
   }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
+@import url('./assets/styles/variables.scss');
+@import url('./assets/styles/reset.scss');
+
+
+MenuBar {
+  width: 50vw;
+  height: 72px;
+}
 
 </style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu">
+    <button class="settings"></button>
+    <button class="sign-in">sign in</button>
+    <button class="contact">contact</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.menu {
+  height: 3rem;
+  width: 100%;
+
+  button {
+    height: 100%;
+    font-size: 1.5em;
+    border: none;
+    vertical-align: top;
+    cursor: pointer;
+  }
+
+  .settings {
+    width: 3rem;
+    background: var(--navy-blue);
+    background-image: url('../assets/icons/settings.png');
+    background-position: center;
+    background-size: 62.5% 62.5%;
+    background-repeat: no-repeat;
+  }
+
+  .sign-in {
+    padding: 0 .9em;
+    background: var(--purple);
+  }
+
+  .contact {
+    padding: 0 .9em;
+    background: var(--strawberry);
+  }
+}
+
+</style>
