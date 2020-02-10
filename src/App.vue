b0cfd6ba0e9360ef93c34716d25ed4e32d12800d
fishu <pkaczoruk@wp.pl>
2020-02-10 13:27:02 +0100

    add method for scrambling cube, send scramble to Scramble component and display it

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <SelectPanel class="select-panel" />
     <MenuBar class="menu-bar" />
     <Stats class="stats" />
-    <Scramble class="scramble" />
+    <Scramble class="scramble" :scramble="scramble" />
     <Display class="display" />
     <Cube class="cube" />
     <Graph class="graph" />
@@ -39,6 +39,75 @@ export default {
     Graph,
     Extra,
     Compete
+  },
+  data() {
+    return {
+      scramble: []
+    }
+  },
+  methods: {
+    generateScramble(cube) {
+      let scramble;
+
+      switch(cube) {
+        case '2x2': 
+        case '3x3': scramble = this.generateScrambleNxN(cube); break;
+      }
+
+      this.scramble = scramble;
+    },
+
+    // function to generate NxNxN scrambles - atm only for 2x2 and 3x3 cubes
+    generateScrambleNxN(cube) {
+      let scramble = [],
+          randSuffix,
+          suffix,
+          letter,
+          randLetter,
+          prev1Letter,
+          prev2Letter,
+          randGroup,
+          prevGroup,
+          length;
+
+      switch(cube) {
+        case '2x2': length = Math.floor(9 + Math.random() * 3); break;
+        case '3x3': length = Math.floor(19 + Math.random() * 6); break;
+      }
+
+      for(let i=0; i<length; i++) {
+        randSuffix = Math.floor(Math.random() * 3);
+
+        // generate letter
+        do {
+          randLetter = Math.floor(Math.random() * 2);
+          randGroup = Math.floor(Math.random() * 3);
+
+          switch(randGroup) {
+            case 0: letter = randLetter === 0 ? 'U' : 'D'; break;
+            case 1: letter = randLetter === 0 ? 'F' : 'B'; break;
+            case 2: letter = randLetter === 0 ? 'R' : 'L'; break;
+          }
+
+        } while(letter === prev1Letter || (letter === prev2Letter && randGroup === prevGroup));
+
+        // determine suffix
+        suffix = randSuffix === 0 ? '' : randSuffix === 1 ? '2' : '\'';
+
+        // save previous and second previous letter
+        if(i>1) prev2Letter = prev1Letter;
+        prev1Letter = letter;
+        prevGroup = randGroup;
+
+        // apply new letter with suffix
+        scramble.push(`${letter}${suffix}`)
+      }
+      
+      return scramble;
+    }
+  },
+  created() {
+    this.generateScramble('3x3');
   }
 }
 </script>
--- src/components/Scramble.vue
+++ src/components/Scramble.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="scramble">
-    <p>U' D R L2 D L' B R2 U2 B F' R U2 B L U' F' L' F2 B' R F D R</p>
+  <div class="scramble" >
+    <p>{{ scramble.join(' ') }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Scramble'
+  name: 'Scramble',
+  props: ['scramble']
 }
 </script>
 
